440f371606d5a6811968854a60fbc635be79d201
Ginett Rosales <g.rosales@uniandes.edu.co>
2022-11-01 22:57:13 -0500

    Update user.entity.ts

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -1,4 +1,7 @@
-import { Column, PrimaryGeneratedColumn } from 'typeorm';
+import {Column,Entity,PrimaryGeneratedColumn,OneToMany,ManyToOne,} from 'typeorm';
+import { RestauranteEntity } from '../restaurante/restaurante.entity';
+import { PaisEntity } from '../pais/pais.entity';
+import { Field, ObjectType } from '@nestjs/graphql';
 
 export class UserEntity {
 
